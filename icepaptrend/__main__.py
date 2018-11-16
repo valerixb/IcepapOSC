29973522b3749adf1edf97f16ac7b2fbe831a46f
Roberto Javier Homs Puron <rhoms@cells.es>
2018-11-16 11:31:26 +0100

    Add license to the code

--- icepaptrend/__main__.py
+++ icepaptrend/__main__.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+# -----------------------------------------------------------------------------
+# This file is part of IcePAPTrend link:
+#        https://github.com/ALBA-Synchrotron/IcePAPTrend
+#
+# Copyright 2017:
+#       MAX IV Laboratory, Lund, Sweden
+#       CELLS / ALBA Synchrotron, Bellaterra, Spain
+#
+# Distributed under the terms of the GNU General Public License,
+# either version 3 of the License, or (at your option) any later version.
+# See LICENSE.txt for more info.
+#
+# You should have received a copy of the GNU General Public License
+# along with IcePAPTrend. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
 
 import sys
 from PyQt4 import QtGui
