7682029b0034ed9fdb80e382bcbdbaf353077885
AntoineDupre <antoine.dupre@maxiv.lu.se>
2020-03-04 17:38:09 +0100

    Adjust axis

--- icepaposc/window_main.py
+++ icepaposc/window_main.py
@@ -364,6 +364,12 @@ class WindowMain(QtGui.QMainWindow):
         self._add_signal(drv_addr, 'StatMoving', 3)
         self._add_signal(drv_addr, 'StatSettling', 3)
         self._add_signal(drv_addr, 'StatOutofwin', 3)
+        self._add_signal(drv_addr, 'StatWarning', 3)
+        self.view_boxes[0].enableAutoRange(axis=self.view_boxes[0].YAxis)
+        self.view_boxes[1].disableAutoRange(axis=self.view_boxes[1].YAxis)
+        self.view_boxes[1].setYRange(-30, 70, padding=0)
+        self.view_boxes[2].disableAutoRange(axis=self.view_boxes[2].YAxis)
+        self.view_boxes[2].setYRange(-1, 20, padding=0)
 
     def _signals_currents(self):
         """Display a specific set of curves."""
@@ -377,7 +383,6 @@ class WindowMain(QtGui.QMainWindow):
         self.view_boxes[1].disableAutoRange(axis=self.view_boxes[1].YAxis)
         self.view_boxes[1].setYRange(-9, 10, padding=0)
         self.view_boxes[2].enableAutoRange(axis=self.view_boxes[2].YAxis)
-
 
     def _signals_target(self):
         """Display a specific set of curves."""
