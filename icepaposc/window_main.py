7236c1952f2b204bb24fa83e4875169670d90c15
AntoineDupre <antoine.dupre@maxiv.lu.se>
2020-03-04 17:35:18 +0100

    Adjust pading

--- icepaposc/window_main.py
+++ icepaposc/window_main.py
@@ -372,6 +372,12 @@ class WindowMain(QtGui.QMainWindow):
         self._add_signal(drv_addr, 'PosAxis', 1)
         self._add_signal(drv_addr, 'MeasI', 2)
         self._add_signal(drv_addr, 'MeasVm', 3)
+        # Ajust plot axis
+        self.view_boxes[0].enableAutoRange(axis=self.view_boxes[0].YAxis)
+        self.view_boxes[1].disableAutoRange(axis=self.view_boxes[1].YAxis)
+        self.view_boxes[1].setYRange(-9, 10, padding=0)
+        self.view_boxes[2].enableAutoRange(axis=self.view_boxes[2].YAxis)
+
 
     def _signals_target(self):
         """Display a specific set of curves."""
