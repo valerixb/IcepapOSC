363962739b5d10503c9f778f518481886cc38577
Roberto Javier Homs Puron <rhoms@cells.es>
2020-10-07 09:55:07 +0200

    Fix flake8 errors

--- icepaposc/window_main.py
+++ icepaposc/window_main.py
@@ -390,13 +390,13 @@ class WindowMain(QMainWindow):
 
         self._add_signal(drv_addr, 'StatOutofwin', 3, True)
 
-        #self._add_signal(drv_addr, 'StatOutofwin', 3)
-        #self._add_signal(drv_addr, 'StatWarning', 3)
-        #self.view_boxes[0].enableAutoRange(axis=self.view_boxes[0].YAxis)
-        #self.view_boxes[1].disableAutoRange(axis=self.view_boxes[1].YAxis)
-        #self.view_boxes[1].setYRange(-30, 70, padding=0)
-        #self.view_boxes[2].disableAutoRange(axis=self.view_boxes[2].YAxis)
-        #self.view_boxes[2].setYRange(-1, 20, padding=0)
+        # self._add_signal(drv_addr, 'StatOutofwin', 3)
+        # self._add_signal(drv_addr, 'StatWarning', 3)
+        # self.view_boxes[0].enableAutoRange(axis=self.view_boxes[0].YAxis)
+        # self.view_boxes[1].disableAutoRange(axis=self.view_boxes[1].YAxis)
+        # self.view_boxes[1].setYRange(-30, 70, padding=0)
+        # self.view_boxes[2].disableAutoRange(axis=self.view_boxes[2].YAxis)
+        # self.view_boxes[2].setYRange(-1, 20, padding=0)
 
     def _signals_currents(self):
         """Display a specific set of curves."""
@@ -406,12 +406,12 @@ class WindowMain(QMainWindow):
         self._add_signal(drv_addr, 'MeasI', 2)
 
         self._add_signal(drv_addr, 'MeasVm', 3, True)
-        #self._add_signal(drv_addr, 'MeasVm', 3)
-        # Ajust plot axis
-        #self.view_boxes[0].enableAutoRange(axis=self.view_boxes[0].YAxis)
-        #self.view_boxes[1].disableAutoRange(axis=self.view_boxes[1].YAxis)
-        #self.view_boxes[1].setYRange(-9, 10, padding=0)
-        #self.view_boxes[2].enableAutoRange(axis=self.view_boxes[2].YAxis)
+        # self._add_signal(drv_addr, 'MeasVm', 3)
+        # # Ajust plot axis
+        # self.view_boxes[0].enableAutoRange(axis=self.view_boxes[0].YAxis)
+        # self.view_boxes[1].disableAutoRange(axis=self.view_boxes[1].YAxis)
+        # self.view_boxes[1].setYRange(-9, 10, padding=0)
+        # self.view_boxes[2].enableAutoRange(axis=self.view_boxes[2].YAxis)
 
 
     def _signals_target(self):
