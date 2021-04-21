7f25535865e1ab5f8b62624df2947f1088b11aea
Roberto Javier Homs Puron <rhoms@cells.es>
2021-04-21 16:07:42 +0200

    Reorganize icons

---
+++ icepaposc/ui/icons/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------------------------
+# This file is part of IcepapOCS link:
+#        https://github.com/ALBA-Synchrotron/IcepapOCS
+#
+# Copyright 2017:
+#       MAX IV Laboratory, Lund, Sweden
+#       CELLS / ALBA Synchrotron, Bellaterra, Spain
+#
+# Distributed under the terms of the GNU General Public License,
+# either version 3 of the License, or (at your option) any later version.
+# See LICENSE.txt for more info.
+#
+# You should have received a copy of the GNU General Public License
+# along with IcepapOCS. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
